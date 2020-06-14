15969c05fe4628508a80bc1e15525925d4aace0c
javierbrea <javier.brea@gmail.com>
2020-06-14 12:30:31 +0200

    chore(deps): Update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,23 @@
-const uglifier = require("rollup-plugin-uglify");
-const commonjs = require("rollup-plugin-commonjs");
-const resolve = require("rollup-plugin-node-resolve");
-const babel = require("rollup-plugin-babel");
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import babel from "@rollup/plugin-babel";
+import { terser } from "rollup-plugin-terser";
 
 const BASE_CONFIG = {
-  input: "index.js"
+  input: "index.js",
 };
 
 const BASE_PLUGINS = [
   resolve({
     mainFields: ["module", "main"],
     browser: true,
-    preferBuiltins: true
+    preferBuiltins: true,
   }),
   commonjs(),
   babel({
     babelrc: false,
-    presets: ["@babel/env"]
-  })
+    presets: ["@babel/env"],
+  }),
 ];
 
 module.exports = [
@@ -25,25 +25,25 @@ module.exports = [
     ...BASE_CONFIG,
     output: {
       file: "dist/index.cjs.js",
-      format: "cjs"
+      format: "cjs",
     },
-    plugins: BASE_PLUGINS
+    plugins: BASE_PLUGINS,
   },
   {
     ...BASE_CONFIG,
     output: {
       file: "dist/index.umd.js",
       format: "umd",
-      name: "pluginAdminApiPaths"
+      name: "pluginAdminApiPaths",
     },
-    plugins: [...BASE_PLUGINS, uglifier.uglify()]
+    plugins: [...BASE_PLUGINS, terser()],
   },
   {
     ...BASE_CONFIG,
     output: {
       file: "dist/index.esm.js",
-      format: "esm"
+      format: "esm",
     },
-    plugins: BASE_PLUGINS
-  }
+    plugins: BASE_PLUGINS,
+  },
 ];

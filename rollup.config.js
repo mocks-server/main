e6a7044fce448034f2461c55aa65a59694d2ceaa
javierbrea <javier.brea@gmail.com>
2020-06-14 18:44:47 +0200

    chore(deps): Update versions

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,32 @@
-const uglifier = require("rollup-plugin-uglify");
-const commonjs = require("@rollup/plugin-commonjs");
-const resolve = require("@rollup/plugin-node-resolve");
-const babel = require("rollup-plugin-babel");
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import babel from "@rollup/plugin-babel";
+import { terser } from "rollup-plugin-terser";
 
 const BASE_CONFIG = {
   input: "index.js",
-  external: ["@data-provider/core", "@data-provider/axios", "@mocks-server/admin-api-paths"]
+  external: ["@data-provider/core", "@data-provider/axios", "@mocks-server/admin-api-paths"],
 };
 
 const GLOBALS = {
   "@data-provider/core": "dataProvider",
   "@data-provider/axios": "dataProviderAxios",
-  "@mocks-server/admin-api-paths": "pluginAdminApiPaths"
+  "@mocks-server/admin-api-paths": "pluginAdminApiPaths",
 };
 
 const BASE_PLUGINS = [
   resolve({
     mainFields: ["module", "main", "jsnext"],
     browser: true,
-    preferBuiltins: true
+    preferBuiltins: true,
   }),
   commonjs({
-    include: "node_modules/**"
+    include: "node_modules/**",
   }),
   babel({
     babelrc: false,
-    presets: ["@babel/env"]
-  })
+    presets: ["@babel/env"],
+  }),
 ];
 
 module.exports = [
@@ -34,9 +34,9 @@ module.exports = [
     ...BASE_CONFIG,
     output: {
       file: "dist/index.cjs.js",
-      format: "cjs"
+      format: "cjs",
     },
-    plugins: BASE_PLUGINS
+    plugins: BASE_PLUGINS,
   },
   {
     ...BASE_CONFIG,
@@ -44,17 +44,17 @@ module.exports = [
       file: "dist/index.umd.js",
       format: "umd",
       name: "mocksServerAdminApiClientDataProvider",
-      globals: GLOBALS
+      globals: GLOBALS,
     },
-    plugins: [...BASE_PLUGINS, uglifier.uglify()]
+    plugins: [...BASE_PLUGINS, terser()],
   },
   {
     ...BASE_CONFIG,
     output: {
       file: "dist/index.esm.js",
       format: "esm",
-      globals: GLOBALS
+      globals: GLOBALS,
     },
-    plugins: BASE_PLUGINS
-  }
+    plugins: BASE_PLUGINS,
+  },
 ];

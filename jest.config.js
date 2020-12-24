cb6127d9275ff75e455ac96a3dcdbf9f44577601
javier.brea <javier.brea@gmail.com>
2020-12-24 12:38:00 +0100

    test(e2e): Fix E2E tests

--- jest.config.js
+++ jest.config.js
@@ -25,7 +25,7 @@ module.exports = {
   },
 
   // The glob patterns Jest uses to detect test files
-  testMatch: ["**/test/unit/**/?(*.)+(spec|test).js?(x)"],
+  testMatch: ["<rootDir>/test/unit/**/*.spec.js"],
   // testMatch: ["**/test/unit/**/Inquirer.spec.js"],
 
   // The test environment that will be used for testing

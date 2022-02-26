63e1b4dbe8677cde5c5299305f969837dd96a13f
javierbrea <javier.brea@gmail.com>
2022-02-26 11:47:55 +0100

    chore(workflow): Remove coverage threshold to check codecov status check

--- packages/main-copy/jest.config.js
+++ packages/main-copy/jest.config.js
@@ -15,14 +15,6 @@ module.exports = {
   coverageDirectory: "coverage",
 
   // An object that configures minimum threshold enforcement for coverage results
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
-    },
-  },
 
   coverageReporters: [["lcov", { projectRoot: "../../" }], "text-summary"],
 

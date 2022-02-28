2ef3781947ca9b25130528aea787ab941180cc90
javierbrea <javier.brea@gmail.com>
2022-02-28 12:36:24 +0100

    chore(workspace): Copy license and notice files to main package in build command

--- packages/main/scripts/commands/build.mjs
+++ packages/main/scripts/commands/build.mjs
@@ -1,4 +1,12 @@
 import { PROJECT_NAME } from "../constants.mjs";
 import { copyWorkspaceFileToProject } from "../../../../scripts/projects/utils.js";
 
-copyWorkspaceFileToProject(PROJECT_NAME, "README.md");
+async function build() {
+  await Promise.all([
+    copyWorkspaceFileToProject(PROJECT_NAME, "README.md"),
+    copyWorkspaceFileToProject(PROJECT_NAME, "LICENSE"),
+    copyWorkspaceFileToProject(PROJECT_NAME, "NOTICE"),
+  ]);
+}
+
+build();

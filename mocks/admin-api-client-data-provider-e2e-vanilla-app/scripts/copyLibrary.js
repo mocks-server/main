f836485a29984b6fbb450a16b91925bba1719efa
javierbrea <javier.brea@gmail.com>
2022-03-25 11:08:36 +0100

    chore(monorepo): adapt admin-api-client-data-provider e2e tests to monorepo

--- mocks/admin-api-client-data-provider-e2e-vanilla-app/scripts/copyLibrary.js
+++ mocks/admin-api-client-data-provider-e2e-vanilla-app/scripts/copyLibrary.js
@@ -1,7 +1,16 @@
 const path = require("path");
 const fsExtra = require("fs-extra");
 
-const libPath = path.resolve(__dirname, "..", "..", "..", "..", "dist", "index.umd.js");
+const libPath = path.resolve(
+  __dirname,
+  "..",
+  "..",
+  "..",
+  "packages",
+  "admin-api-client-data-provider",
+  "dist",
+  "index.umd.js"
+);
 const publicJsPath = path.resolve(__dirname, "..", "public", "js");
 
 const copyLib = () => {

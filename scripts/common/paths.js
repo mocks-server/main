f0fb0840ef8ceb02d7fd0503a0f49e1ae3cb3cfa
javierbrea <javier.brea@gmail.com>
2022-03-26 10:21:51 +0100

    chore: trace paths

--- scripts/common/paths.js
+++ scripts/common/paths.js
@@ -9,5 +9,6 @@ export const ROOT_PATH = path.resolve(__DIRNAME, "..", "..");
 export const NX_WORKSPACE_CONFIG = path.resolve(ROOT_PATH, "nx.json");
 
 export function workspacePath(relativePath) {
+  console.log({ relativePath, ROOT_PATH, NX_WORKSPACE_CONFIG });
   return path.resolve(ROOT_PATH, relativePath);
 }

3b93f40f1c9a82f5e67225615859e080fa1bce48
javierbrea <javier.brea@gmail.com>
2022-07-28 11:12:21 +0200

    chore: Run all core E2E tests

--- test/core-e2e/jest.config.js
+++ test/core-e2e/jest.config.js
@@ -6,7 +6,7 @@ module.exports = {
   clearMocks: true,
 
   testMatch: ["<rootDir>/src/**/*.spec.js"],
-  testMatch: ["<rootDir>/src/**/static.spec.js"],
+  // testMatch: ["<rootDir>/src/**/load-collections-and-routes.spec.js"],
 
   // Indicates whether the coverage information should be collected while executing the test
   collectCoverage: false,
